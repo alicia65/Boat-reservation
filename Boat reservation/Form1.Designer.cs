7f1badcf5cd90f2f9a0bcae5a432c54a9eac54bb
alicia65 <alicia65>
2020-02-16 00:24:55 -0600

    set tab stops.

--- Boat reservation/Form1.Designer.cs
+++ Boat reservation/Form1.Designer.cs
@@ -37,16 +37,20 @@
             this.txtNumberOfChildren = new System.Windows.Forms.Label();
             this.txtAdults = new System.Windows.Forms.TextBox();
             this.txtChildren = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReserve = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnReservation
             // 
             this.btnReservation.Enabled = false;
-            this.btnReservation.Location = new System.Drawing.Point(25, 166);
+            this.btnReservation.Location = new System.Drawing.Point(25, 176);
             this.btnReservation.Name = "btnReservation";
-            this.btnReservation.Size = new System.Drawing.Size(131, 36);
-            this.btnReservation.TabIndex = 0;
-            this.btnReservation.Text = "Reservation";
+            this.btnReservation.Size = new System.Drawing.Size(184, 26);
+            this.btnReservation.TabIndex = 3;
+            this.btnReservation.Text = "Check Reservation";
             this.btnReservation.UseVisualStyleBackColor = true;
             // 
             // cbxboat
@@ -55,7 +59,7 @@
             this.cbxboat.Location = new System.Drawing.Point(215, 10);
             this.cbxboat.Name = "cbxboat";
             this.cbxboat.Size = new System.Drawing.Size(121, 26);
-            this.cbxboat.TabIndex = 1;
+            this.cbxboat.TabIndex = 0;
             this.cbxboat.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
@@ -79,6 +83,7 @@
             // 
             this.txtTotalOfRenters.Location = new System.Drawing.Point(215, 139);
             this.txtTotalOfRenters.Name = "txtTotalOfRenters";
+            this.txtTotalOfRenters.ReadOnly = true;
             this.txtTotalOfRenters.Size = new System.Drawing.Size(121, 24);
             this.txtTotalOfRenters.TabIndex = 4;
             // 
@@ -105,20 +110,60 @@
             this.txtAdults.Location = new System.Drawing.Point(215, 58);
             this.txtAdults.Name = "txtAdults";
             this.txtAdults.Size = new System.Drawing.Size(121, 24);
-            this.txtAdults.TabIndex = 7;
+            this.txtAdults.TabIndex = 1;
             // 
             // txtChildren
             // 
             this.txtChildren.Location = new System.Drawing.Point(215, 104);
             this.txtChildren.Name = "txtChildren";
             this.txtChildren.Size = new System.Drawing.Size(121, 24);
-            this.txtChildren.TabIndex = 8;
+            this.txtChildren.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(215, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnReserve
+            // 
+            this.btnReserve.Location = new System.Drawing.Point(57, 248);
+            this.btnReserve.Name = "btnReserve";
+            this.btnReserve.Size = new System.Drawing.Size(99, 23);
+            this.btnReserve.TabIndex = 4;
+            this.btnReserve.Text = "Reserve";
+            this.btnReserve.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(177, 248);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(297, 248);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 226);
+            this.ClientSize = new System.Drawing.Size(434, 313);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnReserve);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtChildren);
             this.Controls.Add(this.txtAdults);
             this.Controls.Add(this.txtNumberOfChildren);
@@ -149,6 +194,10 @@
         private System.Windows.Forms.Label txtNumberOfChildren;
         private System.Windows.Forms.TextBox txtAdults;
         private System.Windows.Forms.TextBox txtChildren;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReserve;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
